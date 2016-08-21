61363d39bed2839a6dec4d1fc2994e7c370a0b96
Yago <y_rodrigez@hotmail.com>
2016-08-21 17:19:37 +0200

    :knife: unused imports

--- src/main/java/Loader.java
+++ src/main/java/Loader.java
@@ -1,7 +1,4 @@
 import javafx.application.Application;
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
-import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Task;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,7 +6,6 @@ import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
